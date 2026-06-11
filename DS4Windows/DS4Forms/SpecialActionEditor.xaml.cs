3a7e45508dabe50e316034e34db4b35cca0b3769
strifeforlyfe <hbashton13@gmail.com>
2026-06-11 13:34:36 -0500

    Show full launch program path in special actions

--- DS4Windows/DS4Forms/SpecialActionEditor.xaml.cs
+++ DS4Windows/DS4Forms/SpecialActionEditor.xaml.cs
@@ -505,7 +505,7 @@ namespace DS4WinWPF.DS4Forms
             dialog.Multiselect = false;
             dialog.AddExtension = true;
             dialog.DefaultExt = ".exe";
-            dialog.Filter = "Exe (*.exe)|*.exe|Batch (*.bat,*.cmd)|*.bat;*.cmd|All Files (*.*)|*.*";
+            dialog.Filter = "Exe (*.exe)|*.exe|Scripts (*.bat,*.cmd,*.ps1)|*.bat;*.cmd;*.ps1|All Files (*.*)|*.*";
             dialog.Title = "Select Program";
 
             dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);

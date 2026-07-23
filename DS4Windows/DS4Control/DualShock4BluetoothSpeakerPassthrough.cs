16ec5db45b678c23dbed098bcff1ce3c8a59911a
strifeforlyfe <hbashton13@gmail.com>
2026-07-23 02:12:00 -0500

    Revert "Fix in-order DS4 queue guard"
    This reverts commit b8860b263ecb8ab27359b17774e2513c3679454e.

--- DS4Windows/DS4Control/DualShock4BluetoothSpeakerPassthrough.cs
+++ DS4Windows/DS4Control/DualShock4BluetoothSpeakerPassthrough.cs
@@ -1269,7 +1269,6 @@ namespace DS4Windows
                     }
 
                     directDriftCorrectionEnabled = false;
-                    int bufferedFrames;
                     lock (syncRoot)
                     {
                         bufferedFrames = encodedFrames.Count;
